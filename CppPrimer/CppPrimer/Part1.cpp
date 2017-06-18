373cd4db74f2d25f7209a0850ecc434647a99dae
XuanZhang He <609630908@qq.com>
2017-06-18 09:44:53 +0800

    commit

---
+++ CppPrimer/CppPrimer/Part1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "choose.h"
+
+#ifndef PART1
+
+int main() {
+	int nums;
+	std::cout << "Hello, World" << std::endl;
+	std::cin >> nums;
+	return EXIT_SUCCESS;
+}
+
+#endif
---
+++ CppPrimer/CppPrimer/choose.h
@@ -0,0 +1,12 @@
+#ifndef CHOOSE_H_
+#define CHOOSE_H_
+
+#define PART1
+
+#undef	PART1
+
+
+
+
+
+#endif
