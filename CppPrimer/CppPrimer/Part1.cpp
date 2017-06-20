fecdb86bb0de8d60a227b44cb89c7c18ad5c2293
XuanZhang He <609630908@qq.com>
2017-06-20 14:45:03 +0800

    Learning Cpp

--- CppPrimer/CppPrimer/Part1.cpp
+++ CppPrimer/CppPrimer/Part1.cpp
@@ -7,11 +7,12 @@
 
 #define PART1_T1
 #define PART1_T2
+#define PART1_T3
 /*
 	Choose from here
 */
 
-#undef PART1_T2
+#undef PART1_T3
 
 
 #ifndef PART1_T1
@@ -65,5 +66,90 @@ int main() {
 }
 
 #endif
+
+
+#ifndef PART1_T3
+
+/************************************************************************/
+/* 
+1.23 编写程序, 读取多条销售记录, 并记录每个ISBN(每本书)有几条销售记录
+*/
+/************************************************************************/
+
+#include "Sales_item.h"
+
+#include <vector>
+
+#define SALES_ITEM_SIZE		32
+
+typedef enum _TableState
+{
+	TABLE_STATE_NEW_ELEMENT = 0,
+	TABLE_STATE_OLD_ELEMENT = 1,
+
+}tableState;
+
+/************************************************************************/
+/* 
+	ElementType Have "==" function 
+*/
+/************************************************************************/
+template<typename ElementType> class CounterTable {
+public:
+	tableState add(ElementType element) {
+		int counter = 0;
+		int ElementListSize = (int)ElementList.size();
+		for (; counter < ElementListSize && (!(ElementList[counter] == element)); counter++);
+		if (counter >= ElementListSize) {
+			ElementList.push_back(element);
+			ElementNumsList.push_back(1);
+			return TABLE_STATE_NEW_ELEMENT;
+		}
+		ElementNumsList[counter]++;
+		return TABLE_STATE_OLD_ELEMENT;
+	}
+	
+	int tableLength() {
+		return (int)ElementList.size();
+	}
+
+	ElementType getElement(int indexOfElement) {
+		return ElementList.at(indexOfElement);
+	}
+
+	int getElementNums(int indexOfElement) {
+		return ElementNumsList.at(indexOfElement);
+	}
+
+private:
+	std::vector<ElementType> ElementList;
+	std::vector<int> ElementNumsList;
+};
+
+
+/************************************************************************/
+/* 
+输入"- -"结束
+不包括双引号
+*/
+/************************************************************************/
+int main() {
+	Sales_item item;
+	CounterTable<std::string> tab;
+
+	while (std::cin >> item) {
+		tab.add(item.isbn());
+	}
+
+	for (int index = 0; index < tab.tableLength(); index++) {
+		std::cout << tab.getElement(index) << " nums: " << tab.getElementNums(index) << std::endl;
+	}
+
+
+	return EXIT_SUCCESS;
+}
+
+#endif // !1
+
 
 #endif
